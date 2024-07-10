5f36e818b77cc6723b66712c1c4e1cdac3f6c325
Farhana79 <56841366+Farhana79@users.noreply.github.com>
2024-07-10 16:49:24 -0400

    Update InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -7,36 +7,37 @@
  * @copyright Copyright (c) 2024
  *
  */
-
 #include "InsertionSort.hpp"
 
-template <class T>
-InsertionSort<T>::InsertionSort(const bool& ascending)
-        : SortingAlgo<T>("Insertion Sort", true, ascending) {}
+// Constructor
+template <typename DataType>
+InsertionSort<DataType>::InsertionSort(const bool& ascending)
+    : SortingAlgo<DataType>(ascending) {}
 
-template <class T>
-void InsertionSort<T>::sort(std::vector<T>& list) {
-    //Resetting
-    this->comparisons_=0;
-    this->swaps_ = 0;
-
-    // Insertion Sort algorithm
-    for (size_t i = 1; i < list.size(); i++) {
-        T key = list[i];
-        int j = i - 1;
-        bool isSwapped = false;
-        // Move elements of list[0..i-1], that are greater than key,to one position ahead of their current position
-        while (j >= 0 && !this->comparator(list[j], key) && list[j] != key) {
-            list[j + 1] = list[j];
-            j--;
-            this->swaps_++;
-            this->comparisons_++;
-            isSwapped = true;
+// Function to sort the data using insertion sort
+template <typename DataType>
+void InsertionSort<DataType>::sort(std::vector<DataType>& data) {
+    this->swaps = 0;
+    this->comparisons = 0;
+    
+    for (size_t i = 1; i < data.size(); ++i) {
+        DataType key = data[i];
+        size_t j = i;
+        while (j > 0 && this->comparator(key, data[j - 1])) {
+            data[j] = data[j - 1];
+            ++this->swaps;
+            --j;
         }
-        list[j + 1] = key;
-        // Increment comparisons for the last comparison in while loop
-        if (j >= 0 || isSwapped) {
-            this->comparisons_++;
+        data[j] = key;
+        if (j != i) {
+            ++this->swaps;
         }
     }
 }
+
+// Function to clone the object
+template <typename DataType>
+SortingAlgo<DataType>* InsertionSort<DataType>::clone() const {
+    return new InsertionSort<DataType>(*this);
+}
+
