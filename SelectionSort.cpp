09ff62d6d851c0986d7f1eb90586971a9aeae55b
Farhana79 <56841366+Farhana79@users.noreply.github.com>
2024-07-10 17:10:32 -0400

    Update SelectionSort.cpp

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,39 +1,44 @@
 #include "SelectionSort.hpp"
 
-// Constructor
-template <typename DataType>
-SelectionSort<DataType>::SelectionSort(const bool& ascending)
-    : SortingAlgo<DataType>(ascending) {}
 
-// Function to sort the data using selection sort
-template <typename DataType>
-void SelectionSort<DataType>::sort(std::vector<DataType>& data) {
-    this->swaps = 0;
-    this->comparisons = 0;
-
-    for (size_t i = 0; i < data.size() - 1; ++i) {
-        size_t minMaxIndex = findMinMax(data, i);
-        if (i != minMaxIndex) {
-            std::swap(data[i], data[minMaxIndex]);
-            ++this->swaps;
+template <class T>
+size_t SelectionSort<T>::findMinMax(const std::vector<T>& list, size_t start) {
+    // Initialize the index of the smallest or largest element as the starting index
+    size_t minmax = start;
+    // Loop through the elements in the list starting from the given start index
+    for (size_t i = start; i < list.size(); i++) {
+        // Increment the number of comparisons made by the algorithm
+        this->comparisons_++;
+        // Check if the element at current index 'i' is smaller or larger than the current smallest or largest
+        if (this->comparator(list[i], list[minmax])) {
+            // If it is smaller or larger, update the index of the smallest or largest element
+            minmax = i;
         }
     }
+    // Return the index of the smallest or largest element found
+    return minmax;
 }
 
-// Function to find the minimum or maximum element from a given start index
-template <typename DataType>
-size_t SelectionSort<DataType>::findMinMax(const std::vector<DataType>& data, size_t start) {
-    size_t minMaxIndex = start;
-    for (size_t i = start + 1; i < data.size(); ++i) {
-        if (this->comparator(data[i], data[minMaxIndex])) {
-            minMaxIndex = i;
+template <class T>
+SelectionSort<T>::SelectionSort(const bool& ascending)
+        : SortingAlgo<T>("Selection Sort", false, ascending) {}
+
+template <class T>
+void SelectionSort<T>::sort(std::vector<T>& list) {
+    size_t n = list.size();
+    this->comparisons_ = 0;
+    this->swaps_ = 0;
+
+    for (size_t i = 0; i < n  && n != 0; ++i){
+        // Get the member to be swapped in i th iteration
+        size_t min_max_idx = findMinMax(list,i);
+
+        if (min_max_idx != i){
+            // swap the ith element with corresponding min/max element in the ith iteration
+            std::swap(list[i], list[min_max_idx]);
+            this->swaps_++;
         }
     }
-    return minMaxIndex;
+
 }
 
-// Function to clone the object
-template <typename DataType>
-SortingAlgo<DataType>* SelectionSort<DataType>::clone() const {
-    return new SelectionSort<DataType>(*this);
-}
