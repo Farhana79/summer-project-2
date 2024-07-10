617a36b4fb372010f193b44003cdaa7a35dc8ee3
Farhana79 <56841366+Farhana79@users.noreply.github.com>
2024-07-10 17:05:47 -0400

    Update SortingAlgo.hpp

--- SortingAlgo.hpp
+++ SortingAlgo.hpp
@@ -10,45 +10,112 @@
 #ifndef SORTING_ALGO_HPP
 #define SORTING_ALGO_HPP
 
+
+#include <string>
 #include <vector>
 
-template <typename DataType>
-class SortingAlgo {
-protected:
-    int swaps;          ///< Number of swaps made during sorting
-    int comparisons;    ///< Number of comparisons made during sorting
-    bool ascending;     ///< Sorting order
+template <class T>
+class SortingAlgo{
+public:
+    /**
+     * @brief Constructor for SortingAlgo class
+     *
+     * @param name: name of the sorting algorithm
+     * @param stable: boolean indicating if the algorithm is stable
+     * @param ascending: boolean indicating the order of sorting
+     */
+    SortingAlgo(const std::string& name, const bool& stable, const bool& ascending);
 
     /**
-     * @brief : Comparator function to compare two elements
-     * @param : const DataType& a - first element
-     * @param : const DataType& b - second element
-     * @return : true if a < b in ascending order or a > b in descending order
+     * @brief Setter for name
+     *
+     * @param name: name of the sorting algorithm
      */
-    bool comparator(const DataType& a, const DataType& b);
+    void setName(const std::string& name);
 
-public:
-    // Constructor
-    SortingAlgo(const bool& ascending);
+    /**
+     * @brief Setter for stable
+     *
+     * @param stable: boolean indicating if the algorithm is stable
+     */
+    void setStable(const bool& stable);
 
-    // Virtual destructor
-    virtual ~SortingAlgo() = default;
+    /**
+     * @brief Setter for ascending
+     *
+     * @param ascending: boolean indicating the order of sorting
+     */
+    void setAscending(const bool& ascending);
 
-    // Pure virtual function for sorting
-    virtual void sort(std::vector<DataType>& data) = 0;
+    /**
+     * @brief Getter for name
+     *
+     * @return name of the sorting algorithm
+     */
+    std::string getName() const;
 
-    // Virtual function to clone the derived class object
-    virtual SortingAlgo<DataType>* clone() const = 0;
+    /**
+     * @brief Getter for stable
+     *
+     * @return boolean indicating if the algorithm is stable
+     */
+    bool isStable() const;
 
-    // Function to set the sorting order
-    void setOrder(const bool& order);
+    /**
+     * @brief Getter for swaps
+     *
+     * @return number of swaps made during sorting
+     */
+    size_t getSwaps() const;
 
-    // Utility functions
-    int getSwaps() const;
-    int getComparisons() const;
-    bool isSorted(const std::vector<DataType>& data) const;
+    /**
+     * @brief Getter for comparisons
+     *
+     * @return number of comparisons made during sorting
+     */
+    size_t getComparisons() const;
+
+    /**
+     * @brief Getter for ascending
+     *
+     * @return boolean indicating the order of sorting
+     */
+    bool isAscending() const;
+
+    /**
+     * @brief Check if list is sorted in ascending or descending order
+     *
+     * @pre if (ascending_ == true), check if the given parameter is sorted in ascending order
+     * @pre if (ascending_ == false), check if the given parameter is sorted in descending order
+     * @param list: reference to a vector of type T
+     * @return boolean indicating if the list is sorted
+     */
+    bool isSorted(std::vector<T>& list) const;
+
+    /**
+     * @brief Virtual function for sorting the list
+     *
+     * @param list: reference to a vector of type T
+     */
+    virtual void sort(std::vector<T>& list) = 0;
+protected:
+    std::string name_;
+    size_t swaps_;
+    size_t comparisons_;
+    bool stable_;
+    bool ascending_;
+    /**
+    * @brief Comparator function to compare two elements for sorting
+    *
+    * @param a: first element to compare
+    * @param b: second element to compare
+    * @return if (ascending_ == true), return true if (a < b)
+    *         if (ascending_ == false), return true if (a > b)
+    */
+    bool comparator(const T& a, const T& b) const;
 };
 
 #include "SortingAlgo.cpp"
-#endif // SORTING_ALGO_HPP
+
+#endif //SORTING_ALGO_HPP
 
